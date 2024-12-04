33ddee416059fd4ef6b754519ab8467547bb2a83
samatbelentbay <belentbai_s@bi.group>
2024-12-04 23:35:00 +0500

    feat: add auth main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
+import HomePage from "@/components/ui/Home";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      Home 
-    </div>
+    <>
+      <div className="relative flex min-h-[100svh] flex-col bg-[#CACACA]">
+        <section
+          className="absolute w-full h-[302px] bg-cover bg-no-repeat bg-center scale-x-[-1]"
+          style={{ backgroundImage: "url('/top-background.jpeg')" }}
+        ></section>
+        <section>
+          <HomePage />
+        </section>
+      </div>
+    </>
   );
 }
