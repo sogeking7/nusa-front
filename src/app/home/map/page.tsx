54116d392f0e105c1d9510991c6a6b7bce0cefcb
samatbelentbay <belentbai_s@bi.group>
2024-12-24 22:42:16 +0500

    feat: add circle

--- src/app/home/map/page.tsx
+++ src/app/home/map/page.tsx
@@ -1,5 +1,21 @@
+import { Circle } from "@/components/ui/circle";
 import React from "react";
 
 export default function Page() {
-  return <div>Map</div>;
+  return (
+    <>
+      <h1 className="mb-2 text-white md:mb-6 md:text-3xl">Филиалы</h1>
+      <div className="h-full w-full">
+        <div className="relative h-full w-full">
+          <img src={"/map-kz.png"} alt="" className="ml-auto" />
+          <Circle
+            size="md"
+            primaryText="Балхаш"
+            secondaryText="г. Семей"
+            className="absolute right-[530px] top-[125px] z-10"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
