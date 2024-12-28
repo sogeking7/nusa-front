47a79fb5f6df2ae12bbbed0b784e70acaab72b35
samatbelentbay <belentbai_s@bi.group>
2024-12-28 20:31:34 +0500

    fix: design

--- src/app/home/warehouse/page.tsx
+++ src/app/home/warehouse/page.tsx
@@ -1,10 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { mockData } from "@/features/warehouse/mockData";
 import { StatsCard } from "@/features/warehouse/StatsCard";
 import { FinancialTable } from "@/features/warehouse/Table";
+import { AlignRight, SearchIcon } from "lucide-react";
 
 export default function Page() {
   return (
-    <div className="flex h-full w-full flex-col gap-3 lg:gap-10">
+    <div className="flex h-full w-full flex-col gap-5 lg:gap-10">
       <div className="flex items-start justify-between gap-3 max-lg:flex-col">
         <h1 className="text-white md:text-3xl">Склад</h1>
         <div className="grid gap-3 max-lg:w-full md:grid-cols-3 lg:gap-10">
@@ -12,6 +15,17 @@ export default function Page() {
           <StatsCard title="Общая сумма" value="640" change={-0.45} />
           <StatsCard title="Транспорт" value="640" change={-0.45} />
         </div>
+      </div>
+      <div className="flex w-full items-center gap-3">
+        <h2 className="lg:shrink-0">
+          Материальный отчет по товароматериальным счетам
+        </h2>
+        <Input
+          leftIcon={<SearchIcon size={20} className="text-white/40" />}
+          type="text"
+          placeholder="Поиск по имени сотрудника..."
+          className="w-full"
+        />
       </div>
       <FinancialTable data={mockData} />
     </div>
--- src/features/budget/BudgetCard.tsx
+++ src/features/budget/BudgetCard.tsx
@@ -16,22 +16,26 @@ export const BudgetCard = (props: {
       <CardHeader className="p-4 pb-3">
         <CardTitle>
           <div className="space-y-1 text-white">
-            <h2 className="text-base font-medium">{props.title}</h2>
+            <h2 className="font-bold max-lg:text-xs">{props.title}</h2>
             <div className="h-[1px] w-10 bg-primary-purple transition-all duration-500 group-hover:w-3/4" />
           </div>
         </CardTitle>
       </CardHeader>
       <CardContent className="flex justify-between gap-4">
         <div className="md:w-1/2">
-          <h3 className={cn(props.startClassName, "")}>{props.startText}</h3>
-          <p className="mt-1 text-2xl font-extralight text-white/50">
+          <h3 className={cn(props.startClassName, "max-lg:text-xs")}>
+            {props.startText}
+          </h3>
+          <p className="mt-1 text-base font-extralight text-white/50 lg:text-2xl">
             {props.startBudget}
           </p>
         </div>
         <div className="my-1 min-h-full border-r-[1px] border-r-white/20"></div>
         <div className="md:w-1/2">
-          <h3 className={cn(props.endClassName, "")}>{props.endText}</h3>
-          <p className="mt-1 text-2xl font-extralight text-white/50">
+          <h3 className={cn(props.endClassName, "max-lg:text-xs")}>
+            {props.endText}
+          </h3>
+          <p className="mt-1 text-base font-extralight text-white/50 lg:text-2xl">
             {props.endBudget}
           </p>
         </div>
