c0db53d2b7e51370ee2c5724cb0a2132c7622a87
Kaiyrkhan <kairolla222@gmail.com>
2024-12-30 23:23:43 +0500

    add cards

--- src/app/home/map/page.tsx
+++ src/app/home/map/page.tsx
@@ -1,15 +1,27 @@
+import { BalanceCard } from "@/features/map/BalanceCard";
+import { EventAnalyticsCard } from "@/features/map/EventAnalyticsCard";
 import { MapComponent } from "@/features/map/MapComponent";
 
 import React from "react";
 
 export default function Page() {
   return (
-    <div className="md:relative h-full w-full flex max-md:gap-4 max-md:flex-col">
-      <h1 className="md:absolute md:z-[1000] md:left-0 md:top-0 text-white md:mb-6 md:text-3xl">
-        Филиалы
-      </h1>
+    <div className="flex flex-col gap-4">
+      <div className="md:relative h-full w-full flex max-md:gap-4 max-md:flex-col">
+        <h1 className="md:absolute md:z-10 md:left-0 md:top-0 text-white md:mb-6 md:text-3xl">
+          Филиалы
+        </h1>
+        <MapComponent />
+      </div>
 
-      <MapComponent />
+      <div className="grid grid-cols-12 gap-4">
+        <div className="col-span-4">
+          <BalanceCard />
+        </div>
+        <div className="col-span-8">
+          <EventAnalyticsCard />
+        </div>
+      </div>
     </div>
   );
 }
